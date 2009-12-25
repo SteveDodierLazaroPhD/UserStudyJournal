f82270259708ad42d9e79fdab06eec6f3fd638b6
Seif Lotfy <seif@lotfy.com>
2009-12-25 09:27:12 +0100

    changed the code to let the dayviews request their own events
    original bzr status:
    revno: 14
    committer: Seif Lotfy <seif@lotfy.com>
    branch nick: plexus
    timestamp: Fri 2009-12-25 09:27:12 +0100

--- src/view.py
+++ src/view.py
@@ -20,9 +20,16 @@ except RuntimeError, e:
     print "Unable to connect to Zeitgeist, won't send events. Reason: '%s'" %e
     CLIENT = None
 
-SUBJECT_VIEW = "File View"
+SUBJECT_VIEW = "Type"
 EVENT_VIEW = "Activity View"
 VIEW_TYPE = SUBJECT_VIEW
+
+SORTING = "Recency"
+
+
+CATEGORY_FILTER= {}
+for k in SUPPORTED_SOURCES.keys():
+            CATEGORY_FILTER[k] = True
 
 
 class Portal(gtk.Window):
@@ -46,7 +53,7 @@ class Portal(gtk.Window):
         self.__init_toolbar()
         self.__init_menubar()
         
-        self.vbox.pack_start(self.menu, False, False)
+        #self.vbox.pack_start(self.menu, False, False)
         self.vbox.pack_start(self.toolbar, False, False)
         self.vbox.pack_start(self.notebook, True, True)
         self.vbox.pack_start(self.statusbar, False, False)
@@ -129,27 +136,19 @@ class ActivityView(gtk.VBox):
     def __init__(self):
         gtk.VBox.__init__(self)
         self.zg = CLIENT
+        self.__init_optionsbar()
+        
+        self.days = {}
+        
         self.daysbox = gtk.HBox(True)
         scroll = gtk.ScrolledWindow()
         scroll.add_with_viewport(self.daysbox)
         scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        self.__init_optionsbar()
-        
-        self.category_filter = {}
-        for k in SUPPORTED_SOURCES.keys():
-            self.category_filter[k] = True
-        
         self.pack_start(scroll)
-        self.days = {}
-        
-        scroll = gtk.ScrolledWindow()
-        scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        #self.pack_end(scroll, True, True)
-        scroll.set_border_width(1)
         
         self.range = 3
         self._set_today_timestamp()
-        self.set_view()
+        self.set_views()
         
     def __init_optionsbar(self):
         self.optionsbar = gtk.HBox()
@@ -173,10 +172,10 @@ class ActivityView(gtk.VBox):
         
     
     def set_filter(self, widget):
-        self.category_filter[widget.category] = widget.active
+        CATEGORY_FILTER[widget.category] = widget.active
         print widget.category, widget.active
         for daybox in self.days.values():
-            daybox.view.set_filters(self.category_filter)
+            daybox.view.set_filters(CATEGORY_FILTER)
     
     def toggle_optionsbar(self, widget):
         if widget.get_active():
@@ -191,8 +190,7 @@ class ActivityView(gtk.VBox):
         self.start = start
         self.end = end
         self.range = int(int((end - start))/86400) + 1
-        print self.range
-        self.set_view()
+        self.set_views()
     
     def _set_today_timestamp(self, dayinfocus=None):    
         '''
@@ -205,95 +203,93 @@ class ActivityView(gtk.VBox):
             print self.range, dayinfocus, "--------------> "+ pt
         self.end =  dayinfocus + 86399 
         self.start =  dayinfocus - (self.range-1)*86400
-        self.set_view()
+        self.set_views()
       
-            
+    def set_views(self):
+        for w in self.daysbox:
+            self.daysbox.remove(w)
+        for i in xrange(self.range):
+            ptime =  datetime.datetime.fromtimestamp(self.start + i*86400).strftime("%A, %d %B %Y")
+            if not self.days.has_key(ptime):
+                dayview = DayView(ptime, self.start + i*86400)
+                self.days[ptime] = dayview
+            self.daysbox.pack_start(self.days[ptime])
+            self.days[ptime].show_all()
+
+class DayView(gtk.VBox):
         
-    def set_view(self):
+    def __init__(self, ptime, timestamp):
+        gtk.VBox.__init__(self)
+        self.time = ptime
+        self.zg = CLIENT
+        self.start = timestamp
+        self.end = timestamp + 86400
+        self.label = gtk.Label(self.time)
+        if ptime == datetime.datetime.fromtimestamp(time.time()).strftime("%A, %d %B %Y"):
+            self.label.set_markup("<span><b>"+self.time+"</b></span>")
+        scroll = gtk.ScrolledWindow()
+        scroll.set_policy(gtk.POLICY_NEVER, gtk.POLICY_NEVER)
+        scroll.add_with_viewport(self.label)
+        self.pack_start(scroll, False, False)
+        scroll = gtk.ScrolledWindow()
+        scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.view = DayListView()
+        scroll.add_with_viewport(self.view)
+        self.pack_start(scroll)
         event = Event()
         event.set_interpretation(Interpretation.VISIT_EVENT.uri)        
         event2 = Event()
         event2.set_interpretation(Interpretation.MODIFY_EVENT.uri)
-        self.zg.find_event_ids_for_templates([event,event2], self._handle_find_events, [self.start*1000, self.end*1000], num_events=50000, result_type=0)
+        self.zg.find_event_ids_for_templates([event,event2], self._handle_find_events, [self.start*1000, self.end*1000], num_events=50000, result_type=2)
             
     def _handle_find_events(self, ids):
         self.zg.get_events(ids, self._handle_get_events)
     
     def _handle_get_events(self, events):
-        for w in self.daysbox:
-            self.daysbox.remove(w)
-
-        event_dict = {}
-
-        for i in xrange(self.range):
-            ptime =  datetime.datetime.fromtimestamp(self.start + i*86400).strftime("%A, %d %B %Y")
-            if not self.days.has_key(ptime):
-                dayview = DayView(ptime)
-                self.days[ptime] = dayview
-                event_dict[ptime] = []
-                print ptime
-            self.daysbox.pack_start(self.days[ptime])
-        
-        
-        for event in events:
-            t = datetime.datetime.fromtimestamp(float(event.timestamp)/1000).strftime("%A, %d %B %Y")
-            if not event_dict.has_key(t):
-                event_dict[t] = []
-            event_dict[t].append(event)
-            
-        for k in event_dict.keys():
-            dayview = self.days[k]
-            dayview.view.set_filters(self.category_filter)
-            dayview.insert_events(event_dict[k])
-            dayview.show_all()
-
-
-class DayView(gtk.VBox):
-        
-    def __init__(self, ptime):
-        gtk.VBox.__init__(self)
-        self.time = ptime
-        self.label = gtk.Label(self.time)
-        if ptime == datetime.datetime.fromtimestamp(time.time()).strftime("%A, %d %B %Y"):
-            self.label.set_markup("<span><b>"+self.time+"</b></span>")
-        self.pack_start(self.label, False, False)
-        scroll = gtk.ScrolledWindow()
-        scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        self.pack_start(scroll)
-        self.view = DayListView()
-        scroll.add_with_viewport(self.view)
+        self.view.set_filters(CATEGORY_FILTER)
+        self.insert_events(events)
+        self.view.show_all()
         
     def insert_events(self, x):
                 
+        print "-------------------", len(x)
         def exists(uri):
             return not uri.startswith("file://") or os.path.exists(urllib.unquote(str(uri[7:])))
         
         event_dict = {}
         self.view.clear()
-        subjects = []
+        subjects = {}
         
-        if VIEW_TYPE== SUBJECT_VIEW:
-            for event in x:
-                subject = event.subjects[0]
-                
-                if not event_dict.has_key(subject.interpretation):
-                    event_dict[subject.interpretation] = {}
-                if not event_dict[subject.interpretation].has_key(subject.uri):
-                    event_dict[subject.interpretation][subject.uri] = {"count":0, "events":[]}
-                event_dict[subject.interpretation][subject.uri]["count"]+=1
-                event_dict[subject.interpretation][subject.uri]["events"].append(event)
-            for k in event_dict.keys():
-                '''
-                icon = get_category_icon(SUPPORTED_SOURCES[k]["icon"])
-                text = str(len(event_dict[k]))+" "+SUPPORTED_SOURCES[k]["desc"]
-                self.view.append_object(icon, text, event_dict[k])
-                '''
-                for subject in event_dict[k]:
-                    if not subject in subjects:
-                        if exists(subject):
-                            subjects.append(subject)
-                            subject = event_dict[k][subject]["events"][0].subjects[0]
-                            icon =  thumbnailer.get_icon(subject, 32)
-                            self.view.append_object(icon, subject.text, subject)
+        if VIEW_TYPE == SUBJECT_VIEW:
+            if not SORTING == "TYPE":
+                for event in x:
+                    subject = event.subjects[0]
+                    icon =  thumbnailer.get_icon(subject, 32)
+                    self.view.append_object(icon, subject.text, subject)
+
+            else:
+                print "BUAUAU"
+                for event in x:
+                    subject = event.subjects[0]
+                    if not event_dict.has_key(subject.interpretation):
+                        event_dict[subject.interpretation] = {}
+                    if not event_dict[subject.interpretation].has_key(subject.uri):
+                        event_dict[subject.interpretation][subject.uri] = {"count":0, "events":[]}
+                    event_dict[subject.interpretation][subject.uri]["count"]+=1
+                    event_dict[subject.interpretation][subject.uri]["events"].append(event)
+                    for k in event_dict.keys():
+                        for subject in event_dict[k]:
+                            if not subject in subjects.keys():
+                                
+                                    subjects[subject] = event_dict[k][subject]["events"][0].subjects[0]
+                                    subject = event_dict[k][subject]["events"][0].subjects[0]
+                                    icon =  thumbnailer.get_icon(subject, 32)
+                                    self.view.append_object(icon, subject.text, subject)
+
+                        
+
+           
+        elif VIEW_TYPE == EVENT_VIEW:
+            pass
                             
             
