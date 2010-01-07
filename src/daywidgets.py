8f48bd10d9e4ff8235235c91232387ea47182db1
Seif Lotfy <seif@lotfy.com>
2010-01-07 16:48:09 +0100

    changed fonts AGAIN
    original bzr status:
    revno: 102
    committer: Seif Lotfy <seif@lotfy.com>
    branch nick: gnome-activity-journal
    timestamp: Thu 2010-01-07 16:48:09 +0100

--- src/daywidgets.py
+++ src/daywidgets.py
@@ -317,7 +317,7 @@ class DayLabel(gtk.DrawingArea):
         context.select_font_face(self.font_name, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
         
         x, y = event.area.width, event.area.height
-        context.set_font_size(22)
+        context.set_font_size(20)
         if self.leading:
             context.set_source_rgba(1, 1, 1, 1)
         else:
@@ -325,7 +325,7 @@ class DayLabel(gtk.DrawingArea):
             
         xbearing, ybearing, width, height, xadvance, yadvance = context.text_extents(self.day)
         a = (x-width)/2
-        b = y - height - 10
+        b = y - height - 15
         context.move_to(a, b)
         
         context.show_text(self.day)
