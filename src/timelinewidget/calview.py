53229c22ae5302f0f1ff52233406ceb5dd09902f
Seif Lotfy <seif@lotfy.com>
2010-01-10 15:23:47 +0100

    tehk 2010-01-10 More work on Bug #505475     tehk 2010-01-10 Temporary fix for Bug #505475, no idea why it is happening to some people     tehk 2010-01-10 Changed dayrange to selected_range     tehk 2010-01-10 fixed some docstrings     tehk 2010-01-10 fixed copyright header     tehk 2010-01-10 Check for upper and lower selecteds     tehk 2010-01-10 More work on the new selection code, works with indices now     tehk 2010-01-10 More work     tehk 2010-01-10 Changed selected drawing     tehk 2010-01-10 Added the footwork for the new immediate calendar ---------------------------------------------------------- e --include-merged or -n0 to see merged revisions.
    original bzr status:
    revno: 195 [merge]
    committer: Seif Lotfy <seif@lotfy.com>
    branch nick: gnome-activity-journal
    timestamp: Sun 2010-01-10 15:23:47 +0100

--- src/timelinewidget/calview.py
+++ src/timelinewidget/calview.py
@@ -74,12 +74,12 @@ class CairoCalendar(gtk.DrawingArea):
     xincrement = wcolumn + padding
     max_width = xincrement
 
-    def __init__(self, history, dayrange=0):
+    def __init__(self, history, selected_range=0):
         """
 
         Arguments:
         - history: The.CairoCalendars two dimensional list of dates and nitems
-        - dayrange: the number of days displayed at once
+        - selected_range: the number of days displayed at once
         """
         super(CairoCalendar, self).__init__()
         self.add_events(gtk.gdk.BUTTON_PRESS_MASK)
@@ -93,10 +93,15 @@ class CairoCalendar(gtk.DrawingArea):
                            gobject.SIGNAL_RUN_LAST,
                            gobject.TYPE_NONE,())
         self.update_data(history, draw = False)
-        self.dayrange = dayrange
+        self.selected_range = selected_range
 
-    def set_dayrange(self, dayrange):
-        self.dayrange = dayrange
+    def set_selected_range(self, selected_range):
+        """
+        Set the number of days to be colored as selected
+        """
+        self.selected_range = selected_range
+
+    set_dayrange = set_selected_range # Legacy compatibility
 
     def update_data(self, history = None, draw = True):
         """
@@ -118,8 +123,10 @@ class CairoCalendar(gtk.DrawingArea):
     def expose(self, widget, event, selected = None):
         # Default hilight to the last items
         if selected == None:
-            selected = len(self.history) - self.dayrange
-
+            selected = range(len(self.history))[-self.selected_range:]
+        elif isinstance(selected, int):
+            selected = range(selected, selected + self.selected_range)
+            
         context = widget.window.cairo_create()
         # Set the source to the background color
         color = get_gtk_rgba(self.style, "bg", 0, 1.02)
@@ -131,16 +138,25 @@ class CairoCalendar(gtk.DrawingArea):
         context.clip()
         x = self.xincrement
         y = event.area.height
-        color =  get_gtk_rgba(self.style, "text", 1)
+
+        normalcolor =  get_gtk_rgba(self.style, "text", 1)
+        selectedcolor = get_gtk_rgba(self.style, "bg", 3)
 
         months_positions = []
+
+        i = 0
         for date, nitems in self.history:
             if check_for_new_month(date):
                 months_positions += [(date, x)]
+            if i >= selected[0] and i <= selected[-1] and i in selected: 
+                color = selectedcolor
+            else:
+                color = normalcolor
             self.draw_column(context, x, event.area.height, nitems, color)
             x += self.xincrement
+            i += 1
+
         # Draw over the selected items
-        self.draw_selected(context, selected, event.area.height)
         if x > event.area.width: # Check for resize
             self.set_size_request(x+self.xincrement, event.area.height)
         for date, line in months_positions:
@@ -208,27 +224,6 @@ class CairoCalendar(gtk.DrawingArea):
         context.move_to(x + 10, height - self.ypad/3)
         context.show_text(date)
 
-    def draw_selected(self, context, i, height):
-        """
-        hilights a selected area
-
-        Arguments:
-        - context: The drawingarea's cairo context from the expose event
-        - i: The position in history of the object where you want to BEGIN
-             hilighting
-        - height: The event areas height
-        """
-        if i < 0:
-            return # We don't have any data yet
-
-        x = (i * self.xincrement) + self.xincrement
-        y = 0
-
-        color = get_gtk_rgba(self.style, "bg", 3)
-        for n in xrange(self.dayrange):
-            self.draw_column(context, x + (n * self.xincrement), height,
-                self.history[i + n][1], color)
-
     def set_selection(self, i):
         self.connect("expose_event", self.expose, i)
         self.queue_draw()
@@ -260,12 +255,143 @@ class CairoCalendar(gtk.DrawingArea):
         """
         location = int((event.x - self.xincrement) / self.xincrement)
         self.connect("expose_event", self.expose, max(min(location - 2,
-            len(self.history) - self.dayrange), 0))
+            len(self.history) - self.selected_range), 0))
         self.queue_draw()
         self.emit("date-set")
         if callable(self.selection_callback):
             self.selection_callback(self.history, location)
 
+
+class ImmediateCalendar(gtk.DrawingArea):
+    padding = 2
+    ypad = 25
+    wcolumn = 9
+    xincrement = wcolumn + padding
+    max_width = xincrement
+
+    def __init__(self, history):
+        super(ImmediateCalendar, self).__init__()
+        self.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+        self.connect("expose_event", self.expose)
+        self.connect("button-press-event", self.clicked)
+        self.font_name = self.style.font_desc.get_family()
+        gobject.signal_new("idate-set",ImmediateCalendar,
+                           gobject.SIGNAL_RUN_LAST,
+                           gobject.TYPE_NONE,())
+        gobject.signal_new("idata-updated",ImmediateCalendar,
+                           gobject.SIGNAL_RUN_LAST,
+                           gobject.TYPE_NONE,())
+        self.update_data(history, draw = False)
+        
+    def update_data(self, history = None, draw = True):
+        """
+        Sets the objects history attribute or calls update() on the current history object
+        then queues a draw
+        """
+        if history:
+            self.history = history
+            self.largest = 1
+            for date, nitems in self.history:
+                if nitems > self.largest: self.largest = nitems
+            self.max_width = self.xincrement + (self.xincrement *len(history))
+        else:
+            self.history.update()
+        if draw:
+            self.queue_draw()
+        self.emit("data-updated")
+
+    def expose(self, widget, event, selected = False):
+        # Default hilight to the last items
+        context = widget.window.cairo_create()
+        # Set the source to the background color
+        color = get_gtk_rgba(self.style, "bg", 0, 1.02)
+        context.set_source_rgba(*color)
+        context.set_operator(cairo.OPERATOR_SOURCE)
+        context.paint()
+        # set a clip region for the expose event
+        context.rectangle(event.area.x, event.area.y, event.area.width, event.area.height)
+        context.clip()
+        
+        x = self.xincrement
+        y = event.area.height
+        if selected == True:
+            color = get_gtk_rgba(self.style, "bg", 3)
+        else:
+            color =  get_gtk_rgba(self.style, "text", 1)
+
+        for date, nitems in self.history:
+            self.draw_column(context, x, event.area.height, nitems, color)
+            x += self.xincrement
+        
+        # widget size
+        if x > event.area.width: # Check for resize
+            self.set_size_request(x+self.xincrement, event.area.height)
+        self.max_width = x # remove me
+
+    def draw_column(self, context, x, maxheight, nitems, color):
+        """
+        Draws a columns at x with height based on nitems, and maxheight
+
+        Arguments:
+        - context: The drawingarea's cairo context from the expose event
+        - x: The current position in the image
+        - maxheight: The event areas height
+        - nitems: The number of items in the column to be drawn
+        - color: A RGBA tuple
+            Example: (0.3, 0.4, 0.8, 1)
+        """
+        if nitems < 2:
+            nitems = 2
+        maxheight = maxheight - self.ypad
+        height = int(((float(nitems)/self.largest)*(maxheight-2))) - 6
+
+        if height < 2:
+            height = 2
+
+        radius = 1.3
+        y = maxheight - height
+        # Draw
+        context.set_source_rgba(*color)
+        context.move_to(x + radius, y)
+        context.new_sub_path()
+        if nitems > 4:
+            context.arc(radius + x, radius + y, radius, math.pi, 3 * math.pi /2)
+            context.arc(x + self.wcolumn - radius, radius + y, radius, 3 * math.pi / 2, 0)
+            context.rectangle(x, y, self.wcolumn, height)
+        else:
+            context.rectangle(x, y, self.wcolumn, height)
+        context.close_path()
+        context.fill()
+
+    def selection_callback(self, history, i):
+        """
+        A demo callback, either rewrite this or use connect_selection_callback
+        """
+        print "day %s has %s events\n" % (history[i][0], history[i][1])
+
+    def connect_selection_callback(self, callback):
+        """
+        Connect a callback for clicked to call. clicked passes this widget,
+        a history list, and i to the function
+        """
+        if callable(callback):
+            self.selection_callback = callback
+        else:
+            raise TypeError("Callback is not a function")
+
+    def clicked(self, widget, event, *args, **kwargs):
+        """Handles click events
+
+        By wrapping this and using the returned location you can do nifty stuff
+        with the history object
+
+        Calls a calback set by connect_selection_callback
+        """
+        self.connect("expose_event", self.expose, True)
+        self.queue_draw()
+        self.emit("date-set")
+        if callable(self.selection_callback):
+            self.selection_callback(self.history, 0)
 
 class CalendarWidget(gtk.HBox):
     """
@@ -277,6 +403,11 @@ class CalendarWidget(gtk.HBox):
         viewport.set_shadow_type(gtk.SHADOW_NONE)
         viewport.set_size_request(600,70)
         self.calendar = CairoCalendar([[0, 0]])
+        #mini cal setup
+        #self.immediatecalendar = ImmediateCalendar([[0,0]])
+        #self.immediatecalendar.set_size_request(70,70)
+
+        # viewport work
         viewport.add(self.calendar)
         # Aligning work
         align = gtk.Alignment(0,0,1,1)
@@ -301,10 +432,11 @@ class CalendarWidget(gtk.HBox):
 
         self.pack_start(b1, False, False)
         self.pack_start(align, True, True, 3)
+        #self.pack_start(self.immediatecalendar, False, False)
         self.pack_end(b2, False, False)
         # Prepare the adjustment
         self.adjustment = viewport.get_hadjustment()
-        self.adjustment.set_upper(self.calendar.max_width)
+        #self.adjustment.set_upper(self.calendar.max_width)
         self.adjustment.set_value(1) # Needs to be set twice to work
         self.adjustment.set_value(self.calendar.max_width - self.adjustment.page_size)
 
@@ -322,7 +454,7 @@ class CalendarWidget(gtk.HBox):
         adjustment.set_value(newadjval)
 
     def scroll_to_end(self, *args, **kwargs):
-        self.adjustment.set_upper(self.calendar.max_width)
+        #self.adjustment.set_upper(self.calendar.max_width)
         self.adjustment.set_value(1)
         self.adjustment.set_value(self.calendar.max_width - self.adjustment.page_size)
 
