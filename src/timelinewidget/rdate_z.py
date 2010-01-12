576e58fbc0b2d10e4b3beeacfb22b5468f42a5d5
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2010-01-12 23:38:17 +0100

    Cleanup rdate_z.py.
    original bzr status:
    revno: 243
    committer: Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
    branch nick: plexus
    timestamp: Tue 2010-01-12 23:38:17 +0100

--- src/timelinewidget/rdate_z.py
+++ src/timelinewidget/rdate_z.py
@@ -1,7 +1,23 @@
-#!/usr/bin/env python
-#coding:utf-8
-# Author:
-# Purpose: 
+# -.- coding: utf-8 -.-
+#
+# GNOME Activity Journal
+#
+# Copyright © 2010 Seif Lotfy <seif@lotfy.com>
+# Copyright © 2010 Randal Barlow <email.tehk@gmail.com>
+# Copyright © 2010 Siegfried-Angel Gevatter Pujals <siegfried@gevatter.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 iszeitgeist = True
 
@@ -14,7 +30,7 @@ from zeitgeist.datamodel import Event, Subject, Interpretation, Manifestation, \
 CLIENT = ZeitgeistClient()
 
 def datelist(n, callback):
-    today =  int(time.mktime(time.strptime(time.strftime("%d %B %Y") , "%d %B %Y")))
+    today = int(time.mktime(time.strptime(time.strftime("%d %B %Y"), "%d %B %Y")))
     today = today - n*86400 
     
     x = []
@@ -29,12 +45,9 @@ def datelist(n, callback):
             callback(x)
     
     def get_ids(start, end):
-        event = Event()
-        event.set_interpretation(Interpretation.VISIT_EVENT.uri)
-        event2 = Event()
-        event2.set_interpretation(Interpretation.MODIFY_EVENT.uri)
-        
-        event_templates = [event, event2]
+        event_templates = [
+            Event.new_for_values(interpretation=Interpretation.VISIT_EVENT.uri),
+            Event.new_for_values(interpretation=Interpretation.MODIFY_EVENT.uri)]
         CLIENT.find_event_ids_for_templates(event_templates,
             _handle_find_events, [start * 1000, end * 1000],
             num_events=50000, result_type=0)
