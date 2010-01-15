0ac6422c5ea61c468fc70b9c43a044cbc22fa17a
tehk <tehk@tehk-desktop>
2010-01-15 14:52:55 -0500

    moved gtkhistogram code into gtkhistogram.py. histogramview.py now holds all journal theming, and histogram container widget code
    original bzr status:
    revno: 279
    committer: tehk <tehk@tehk-desktop>
    branch nick: gnome-activity-journal
    timestamp: Fri 2010-01-15 14:52:55 -0500

---
+++ src/histogramwidget/gtkhistogram.py
@@ -0,0 +1,367 @@
+# -.- coding: utf-8 -.-
+#
+# GNOME Activity Journal
+#
+# Copyright © 2010 Randal Barlow
+# Copyright © 2010 Markus Korn
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Takes a two dementional tuple or list and turns it into a graph based on
+datastore[n][1]'s size
+
+# datastore = [[raw_date, nitems]]
+"""
+
+import cairo
+import gobject
+import gtk
+import math
+import datetime
+import calendar
+
+
+def check_for_new_month(date):
+    if datetime.date.fromtimestamp(date).day == 1:
+        return True
+    return False
+
+def get_gtk_rgba(style, palette, i, shade = 1):
+    """Takes a gtk style and returns a RGB tuple
+
+    Arguments:
+    - style: a gtk_style object
+    - palette: a string representing the palette you want to pull a color from
+        Example: "bg", "fg"
+    - shade: how much you want to shade the color
+    """
+    f = lambda num: (num/65535.0) * shade
+
+    color = getattr(style, palette)[i]
+    if isinstance(color, gtk.gdk.Color):
+        red = f(color.red)
+        green = f(color.green)
+        blue = f(color.blue)
+
+        return (min(red, 1), min(green, 1), min(blue, 1), 1)
+    else: raise TypeError("Not a valid gtk.gdk.Color")
+
+class TooltipEventBox(gtk.EventBox):
+    _saved_tooltip_location = None
+    def __init__(self, histogram):
+        super(TooltipEventBox, self).__init__()
+        self.add(histogram)
+        self.histogram = histogram
+        self.set_property("has-tooltip", True)
+        self.connect("query-tooltip", self.query_tooltip)
+
+    def query_tooltip(self, widget, x, y, keyboard_mode, tooltip):
+        location = self.histogram.get_data_index_from_cartesian(x, y)
+        if location != self._saved_tooltip_location:
+            # don't show the previous tooltip if we moved to another
+            # location
+            self._saved_tooltip_location = location
+            return False
+        try:
+            timestamp, count = self.histogram.datastore[location]
+        except IndexError:
+            # there is no bar for at this location
+            # don't show a tooltip
+            return False
+        date = datetime.date.fromtimestamp(timestamp).strftime("%Y-%m-%d")
+        tooltip.set_text("%s (%i)" %(date, count))
+        return True
+        
+    
+class CairoHistogram(gtk.DrawingArea):
+    """
+    A histogram which is represented by a list of dimensions and dates
+    """
+    padding = 1
+    bottom_padding = 0
+    top_padding = 14
+    wcolumn = 15
+    xincrement = wcolumn + padding
+    start_x_padding = 2
+    max_width = xincrement
+    column_radius = 0.1
+    font_size = 10
+    
+    datastore = None
+    selected_range = 0
+    highlighted = []
+    __calbacks = None
+    
+    bg_color = (1, 1, 1, 1)
+    column_color_normal =  (1, 1, 1, 1)
+    column_color_selected = (1, 1, 1, 1)
+    column_color_alternative = (1, 1, 1, 1)
+    font_color = (0, 0, 0, 0)
+    stroke_color = (1, 1, 1, 0)
+
+    __last_location = -1
+    
+    __gsignals__ = {
+        # the index of the first selected item in the datastore.
+        "selection-set": (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,(gobject.TYPE_INT,)),
+        "data-updated":  (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,())
+        }
+
+    def __init__(self, datastore = None, selected_range = 0):
+        """
+
+        Arguments:
+        - datastore: The.CairoHistograms two dimensional list of dates and nitems
+        - selected_range: the number of days displayed at once
+        """
+        super(CairoHistogram, self).__init__()
+        self.set_events(gtk.gdk.BUTTON_MOTION_MASK | gtk.gdk.POINTER_MOTION_HINT_MASK | gtk.gdk.BUTTON_RELEASE_MASK | gtk.gdk.BUTTON_PRESS_MASK)
+        self.set_flags(gtk.CAN_FOCUS)
+        self._expose_handler_id = self.connect("expose_event", self.expose)
+        self.connect("button_press_event", self.mouse_interaction)
+        self.connect("motion_notify_event", self.mouse_interaction)
+        self.font_name = self.style.font_desc.get_family()
+        self.set_data(datastore if datastore else [], draw = False)
+        self.selected_range = selected_range
+        self.connect("style-set", self.change_style)
+        
+    def reconnect_expose(self, *args):
+        """ disconnects the current expose_event handler and connects to
+        a new one with given arguments
+        """
+        self.disconnect(self._expose_handler_id)
+        self._expose_handler_id = self.connect("expose_event", self.expose, *args)
+        
+    def query_tooltip(self, widget, x, y, keyboard_mode, tooltip):
+        location = self.get_data_index_from_cartesian(x, y)
+        if location != self._saved_tooltip_location:
+            # don't show the previous tooltip if we moved to another
+            # location
+            self._saved_tooltip_location = location
+            return False
+        try:
+            timestamp, count = self.datastore[location]
+        except IndexError:
+            # there is no bar for at this location
+            # don't show a tooltip
+            return False
+        date = datetime.date.fromtimestamp(timestamp).strftime("%Y-%m-%d")
+        tooltip.set_text("%s (%i)" %(date, count))
+        return True
+    
+    def change_style(self, widget, *args, **kwargs):
+        self.bg_color = get_gtk_rgba(self.style, "base", 0)
+        self.column_color_normal =  get_gtk_rgba(self.style, "text", 4, 1.17)
+        self.column_color_selected = get_gtk_rgba(self.style, "bg", 3)
+        pal = get_gtk_rgba(self.style, "bg", 3, 1.2)
+        self.column_color_alternative = (pal[2], pal[1], pal[0], 1)
+        self.column_color_selected_alternative = get_gtk_rgba(self.style, "bg", 3, 0.6)
+        fg = self.style.fg[gtk.STATE_NORMAL]
+        bg = self.style.bg[gtk.STATE_NORMAL]
+        self.font_color = get_gtk_rgba(self.style, "text", 4)
+        self.stroke_color = get_gtk_rgba(self.style, "text", 4, 0.8)
+
+    def set_selected_range(self, selected_range):
+        """
+        Set the number of days to be colored as selected
+        """
+        self.selected_range = selected_range
+
+    set_dayrange = set_selected_range # Legacy compatibility
+
+    def set_data(self, datastore = None, draw = True):
+        """
+        Sets the objects datastore attribute or calls update() on the current datastore object
+        then queues a draw
+        """
+        if datastore != None and isinstance(datastore, list):
+            self.datastore = datastore
+            self.largest = 1
+            for date, nitems in self.datastore:
+                if nitems > self.largest: self.largest = nitems
+            self.max_width = self.xincrement + (self.xincrement *len(datastore))
+        elif datastore != None and not isinstance(datastore, list):
+            raise TypeError("Datastore is not a list")
+        if draw:
+            self.queue_draw()
+        self.emit("data-updated")
+
+    def get_data(self):
+        return self.datastore
+    
+    def expose(self, widget, event, selected = None, highlighted = None):
+        # Default hilight to the last items
+        if selected == None:
+            selected = range(len(self.datastore))[-self.selected_range:]
+        elif isinstance(selected, int):
+            selected = range(selected, selected + self.selected_range)
+        elif isinstance(selected, list) and len(selected) == 0:
+            selected = [-1] # Disable color
+        
+        context = widget.window.cairo_create()
+        # Set the source to the background color
+        
+        context.set_source_rgba(*self.bg_color)
+        context.set_operator(cairo.OPERATOR_SOURCE)
+        context.paint()
+        # set a clip region for the expose event
+        context.rectangle(event.area.x, event.area.y, event.area.width, event.area.height)
+        context.clip()
+        x = self.start_x_padding
+        y = event.area.height
+
+        months_positions = []
+        i = 0
+        for date, nitems in self.datastore:
+            if check_for_new_month(date):
+                months_positions += [(date, x)]
+            if len(self.highlighted) > 0 and i >= self.highlighted[0] and i <= self.highlighted[-1] and i in self.highlighted: 
+                color = self.column_color_selected_alternative if i in selected else self.column_color_alternative
+            elif i >= selected[0] and i <= selected[-1] and i in selected:
+                color = self.column_color_selected
+            else:
+                color = self.column_color_normal
+            self.draw_column(context, x, event.area.height, nitems, color)
+            x += self.xincrement
+            i += 1
+        # Draw over the selected items
+        if x > event.area.width: # Check for resize
+            self.set_size_request(x+self.xincrement, event.area.height)
+        for date, line in months_positions:
+            self.draw_month(context, line - self.padding, event.area.height, date)
+        self.max_width = x # remove me
+        
+    def draw_column(self, context, x, maxheight, nitems, color):
+        """
+        Draws a columns at x with height based on nitems, and maxheight
+
+        Arguments:
+        - context: The drawingarea's cairo context from the expose event
+        - x: The current position in the image
+        - maxheight: The event areas height
+        - nitems: The number of items in the column to be drawn
+        - color: A RGBA tuple
+            Example: (0.3, 0.4, 0.8, 1)
+        """
+        if nitems < 2:
+            nitems = 2
+        maxheight = maxheight - self.bottom_padding
+        height = int(((float(nitems)/self.largest)*(maxheight-2))) - self.top_padding
+
+        if height < 2:
+            height = 2
+
+        y = maxheight - height
+        # Draw
+        context.set_source_rgba(*color)
+        context.move_to(x + self.column_radius, y)
+        context.new_sub_path()
+        if nitems > 4:
+            context.arc(self.column_radius + x, self.column_radius + y, self.column_radius, math.pi, 3 * math.pi /2)
+            context.arc(x + self.wcolumn - self.column_radius, self.column_radius + y, self.column_radius, 3 * math.pi / 2, 0)
+            context.rectangle(x, y, self.wcolumn, height)
+        else:
+            context.rectangle(x, y, self.wcolumn, height)
+        context.close_path()
+        context.fill()
+
+    def draw_month(self, context, x, height, date):
+        """
+        Draws a line signifying the start of a month
+        """
+        fg = self.style.fg[gtk.STATE_NORMAL]
+        bg = self.style.bg[gtk.STATE_NORMAL]
+        context.set_source_rgba(*self.stroke_color)
+        
+        context.set_line_width(1)
+        context.move_to(x + int(self.padding/2) + 0.5, 0)
+        context.line_to(x + int(self.padding/2) + 0.5, height)
+        context.stroke()
+
+        context.set_source_rgba(*self.font_color)
+        
+        context.select_font_face(self.font_name, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        context.set_font_size(self.font_size)
+
+        date = datetime.date.fromtimestamp(date)
+        month = calendar.month_name[date.month]
+
+        date = "%s %d" % (month, date.year)
+        xbearing, ybearing, width, oheight, xadvance, yadvance = context.text_extents(date)
+        context.move_to(x + 8, oheight+2)
+        context.show_text(date)
+
+    def set_selection(self, i):
+        self.reconnect_expose(i)
+        self.queue_draw()
+        if isinstance(i, int):
+            self.emit("selection-set", max(i, 0))
+        elif isinstance(i, list) and len(i) > 0:
+            self.emit("selection-set", max(i[0], 0))
+
+    def set_highlighted(self, highlighted):
+        if isinstance(highlighted, list):
+            self.highlighted = highlighted
+        else: raise TypeError("highlighted is not a list")
+        self.reconnect_expose()
+        self.queue_draw()
+
+    def clear_highlighted(self):
+        self.highlighted = []
+        self.reconnect_expose()
+        self.queue_draw()
+       
+    def add_selection_callback(self, callback):
+        """
+        add a callback for clicked to call when a item is clicked. 
+        clicked passes this widget, a datastore list, and i to the function
+        """
+        if callable(callback):
+            if not self.__calbacks:
+                self.__calbacks = [callback]
+            elif isinstance(self.__calbacks, list):
+                self.__calbacks.append(callback)
+        else:
+            raise TypeError("Callback is not a function")
+
+    def get_data_index_from_cartesian(self, x, y):
+        return int((x - self.start_x_padding) / self.xincrement)
+   
+    def mouse_interaction(self, widget, event, *args, **kwargs):
+        """
+        Reacts to mouse moving (while pressed), and clicks
+        """
+        location = self.get_data_index_from_cartesian(event.x, event.y)
+        if location != self.__last_location:
+            self.change_location(location)
+        self.__last_location = location
+        return True
+
+    def change_location(self, location):
+        """Handles click events
+
+        By wrapping this and using the returned location you can do nifty stuff
+        with the datastore object
+
+        Calls a calback set by connect_selection_callback
+        """
+        self.reconnect_expose(max(location - self.selected_range + 1, 0))
+        self.queue_draw()
+        self.emit("selection-set", max(location - self.selected_range + 1, 0))
+        if isinstance(self.__calbacks, list):
+            for callback in self.__calbacks:
+                if callable(callback):
+                    callback(self, self.datastore, location)
+
