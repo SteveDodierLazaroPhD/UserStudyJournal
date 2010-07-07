753b3d34cff7565fdd95c4c112f961a36595a1b8
tehk <tehk@tehk-desktop>
2010-07-07 18:20:13 -0400

    src/wireless-dialog.c - (security_combo_init): fix active item handling
    2008-03-29  Dan Williams  <dcbw@redhat.com>
    
    	* src/wireless-dialog.c
    		- (security_combo_init): fix active item handling
    
    svn path=/trunk/; revision=619
    
    original bzr status:
    revno: 497
    author: Dan Williams <dcbw@redhat.com>
    committer: Dan Williams <dcbw@src.gnome.org>
    timestamp: Sat 2008-03-29 20:58:37 +0000

--- src/store.py
+++ src/store.py
@@ -183,9 +183,12 @@ class Day(gobject.GObject):
         CLIENT.find_event_ids_for_templates(self.templates, self.set_ids, self.time_range, num_events=MAXEVENTS)
         CLIENT.install_monitor(self.time_range, self.templates, self.insert_events, self.remove_ids)
         if external.HAMSTER:
-            facts = external.HAMSTER.get_facts(self.start, self.end)
-            for fact in facts:
-                self.insert_events(None, fact.get_events())
+            try:
+                facts = external.HAMSTER.get_facts(self.start, self.end)
+                for fact in facts:
+                    self.insert_events(None, fact.get_events())
+            except TypeError:
+                pass #print "Hamster support disabled temporarely"
 
     def has_id(self, id_):
         """
