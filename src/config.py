959cf84cb3fd23ad8fd720cd6f9cecd2600955de
Siegfried-Angel Gevatter Pujals <siegfried@gevatter.com>
2011-07-16 19:17:47 +0200

    original bzr status: revno: 244 committer: Satyajit Sahoo <satyajit.happy@gmail.com> timestamp: Fri 2014-01-24 00:54:08 +0530 message:

--- src/config.py
+++ src/config.py
@@ -48,7 +48,7 @@ INTERPRETATION_VCS = "aj://vcs" #version control system
 # Installation details
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DATA_PATH = os.path.join(BASE_PATH, "data")
-VERSION = "0.6.0"
+VERSION = "0.8.0"
 GETTEXT_PATH = os.path.join(BASE_PATH, "../locale")
 
 USER_DATA_PATH = BaseDirectory.save_data_path("gnome-activity-journal")
